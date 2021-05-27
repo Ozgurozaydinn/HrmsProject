21885a02db5f4bc3df365698a0403d8fc5175823
Özgür Özaydın <ozgurozaydin10@gmail.com>
2021-05-27 12:05:17 +0300

    Swagger support added.

--- hrms/src/main/java/javacamp/hrms/entities/concretes/JobTitle.java
+++ hrms/src/main/java/javacamp/hrms/entities/concretes/JobTitle.java
@@ -6,10 +6,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 @Data
 @Entity
 @Table(name="job_titles")
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobTitle {
 	
 	@Id
